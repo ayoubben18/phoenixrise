a92a0643fd1794b39f3d0933263d5982ca0ecd6c
ayoubben18 <ayoubbensalah2004@gmail.com>
2024-04-03 01:47:21 +0000

    fix bug import and metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Roboto } from "@next/font/google"
 import "./globals.css";
 import { cn } from "@/lib/utils"
 import { ThemeProvider } from "@/components/theme-provider"
-import {NavBar} from "@/components/NavBar";
 
 
 const roboto = Roboto({
