dc5d5119dd72331bf04d88414ddf740d3055a636
ayoubben18 <ayoubbensalah2004@gmail.com>
2024-04-03 03:15:28 +0000

    fix logo and text blurness

--- src/components/HoverBorderGradientDemo.tsx
+++ src/components/HoverBorderGradientDemo.tsx
@@ -28,7 +28,7 @@ const AceternityLogo = () => {
             viewBox="0 0 66 65"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
-            className="h-3 w-3 text-white"
+            className="h-3 w-3 text-white rotate-90"
         >
             <path
                 d="M8 8.05571C8 8.05571 54.9009 18.1782 57.8687 30.062C60.8365 41.9458 9.05432 57.4696 9.05432 57.4696"
