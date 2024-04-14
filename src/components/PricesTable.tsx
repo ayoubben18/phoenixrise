a649d84ee9dc0cdba2da82c58e0cb8b1fd816cce
ayoubben18 <ayoubbensalah2004@gmail.com>
2024-04-14 15:01:45 +0100

    correct the prices table

--- src/components/PricesTable.tsx
+++ src/components/PricesTable.tsx
@@ -1,87 +1,54 @@
 import {
-    Table,
-    TableBody,
-    TableCaption,
-    TableCell,
-    TableFooter,
-    TableHead,
-    TableHeader,
-    TableRow,
-} from "@/components/ui/table"
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 const invoices = [
-    {
-        invoice: "Landing pages",
-        paymentStatus: "$30",
-        totalAmount: "$80",
-        paymentMethod: "$125",
-    },
-    {
-        invoice: "Logo design",
-        paymentStatus: "$10",
-        totalAmount: "$25",
-        paymentMethod: "$175",
-    },
-    {
-        invoice: "Promotion video (short)",
-        paymentStatus: "$10",
-        totalAmount: "$30",
-        paymentMethod: "$70",
-    },
-    {
-        invoice: "Social media content (30 days)",
-        paymentStatus: "-",
-        totalAmount: "$130",
-        paymentMethod: "$250",
-    },
-    {
-        invoice: "E-commerce store",
-        paymentStatus: "-",
-        totalAmount: "$80",
-        paymentMethod: "$100",
-    },
-    {
-        invoice: "Website conception",
-        paymentStatus: "$80",
-        totalAmount: "$120",
-        paymentMethod: "$300",
-    },
-    {
-        invoice: "Graphic design (1 item)",
-        paymentStatus: "$5",
-        totalAmount: "$30",
-        paymentMethod: "$80",
-    },
-    {
-        invoice: "Short video editing",
-        paymentStatus: "$5",
-        totalAmount: "$20",
-        paymentMethod: "-",
-    },
-]
-
+  {
+    invoice: "Building a website",
+    totalAmount: "40 $/hr",
+  },
+  {
+    invoice: "E-commerce store",
+    totalAmount: "40 $/hr",
+  },
+  {
+    invoice: "Landing pages",
+    totalAmount: "30 $/hr",
+  },
+  {
+    invoice: "Graphic design (posts, logos, etc)",
+    totalAmount: "10 $/hr",
+  },
+  {
+    invoice: "Promotion video (30 s)",
+    totalAmount: "30 $/hr",
+  },
+];
 export function PricesTable() {
-    return (
-        <Table className=' text-xs xs:text-sm mt-12' >
-            <TableCaption>Prices Table</TableCaption>
-            <TableHeader>
-                <TableRow>
-                    <TableHead >Services</TableHead>
-                    <TableHead>Min Price</TableHead>
-                    <TableHead>Average</TableHead>
-                    <TableHead className='text-right'>Max Price</TableHead>
-                </TableRow>
-            </TableHeader>
-            <TableBody>
-                {invoices.map((invoice) => (
-                    <TableRow key={invoice.invoice}>
-                        <TableCell className="font-medium">{invoice.invoice}</TableCell>
-                        <TableCell>{invoice.paymentStatus}</TableCell>
-                        <TableCell>{invoice.paymentMethod}</TableCell>
-                        <TableCell className="text-right">{invoice.totalAmount}</TableCell>
-                    </TableRow>
-                ))}
-            </TableBody>
-        </Table>
-    )
+  return (
+    <Table className=" text-xs xs:text-sm mt-12">
+      <TableCaption>Prices Table</TableCaption>
+      <TableHeader>
+        <TableRow>
+          <TableHead>Services</TableHead>
+          <TableHead className="text-right">Max Price</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {invoices.map((invoice) => (
+          <TableRow key={invoice.invoice}>
+            <TableCell className="font-medium">{invoice.invoice}</TableCell>
+            <TableCell className="text-right">{invoice.totalAmount}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
 }
