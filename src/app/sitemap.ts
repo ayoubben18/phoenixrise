9568e5ebe39217b1b07b2009ea4b856f2b85cda0
ayoubben18 <ayoubbensalah2004@gmail.com>
2024-04-23 09:25:02 +0100

    Update sitemap change frequency for video editing page

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -5,7 +5,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: "https://phoenixrise.agency/",
       lastModified: new Date(),
-      changeFrequency: "daily",
+      changeFrequency: "weekly",
       priority: 1,
     },
     {
@@ -16,6 +16,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
     },
     {
       url: "https://phoenixrise.agency/services/web-dev",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.5,
+    },
+    {
+      url: "https://phoenixrise.agency/services/video-editing",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 0.5,
